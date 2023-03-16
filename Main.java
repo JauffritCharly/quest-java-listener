0b8a4dadeeeabb2052cc091340798192083a83de
charly <jauffrit.charly@hotmail.com>
2023-03-16 10:16:44 +0100

    update

--- Main.java
+++ Main.java
@@ -6,13 +6,11 @@ public class Main {
 
         // TODO : create and use a TransactionListener interface with onComplete method
 
-
         Transactions.withdraw(account, 100, new TransactionListener() {
-            public void onComplete(Bank bank){
+            @Override
+            public void onComplete(Bank account) {
                 System.out.printf("Your balance is: %d%n", account.getTotal());
             }
         });
-
-        
     }
 }
